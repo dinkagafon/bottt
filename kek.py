2b034c4a3c8f746fd0000bade15c15a23abcdf18
dinkagafon <agafonikita@outlook.com>
2019-04-08 17:47:57 +0300

    Update kek.py

--- kek.py
+++ kek.py
@@ -5,7 +5,7 @@ class BotHandler:
 
     def __init__(self, token):
         self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.api_url = "https://api.telegram.org/bot693860523:AAE6lbwLDIoftjFCaOLBAiJ_TAQHuKEYoOc/".format(token)
 
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
